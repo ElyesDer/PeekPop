13e9a21d1e0b541ef1319b76e02a33d36c62cfa1
Elyes <lyesderouich@gmail.com>
2022-12-19 08:34:51 +0100

    Test impl

--- PeekPopTests/PeekPopTests.swift
+++ PeekPopTests/PeekPopTests.swift
@@ -10,12 +10,31 @@ import XCTest
 
 final class PeekPopTests: XCTestCase {
 
+    let zut: HomeViewModel = .init()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_sort_method() {
+        // given
+        zut.randomAddRecipe()
+        zut.randomAddRecipe()
+        
+        // test
+        if let firstElement = zut.recipes.first {
+            let secondElement = zut.recipes[1]
+            
+            XCTAssert(firstElement.note > secondElement.note, "Note is ordered in descanding order")
+            XCTAssert(firstElement.requiredTime < secondElement.requiredTime, "requiredTime is ordered in Ascanding order")
+            XCTAssert(firstElement.name < secondElement.name, "Name is ordered in Ascanding order")
+        } else {
+            assertionFailure("Should have atleast a value")
+        }
     }
 
     func testExample() throws {
